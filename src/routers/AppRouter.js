5da46ef72d031a31ea0a272d45612b41a4f2e299
mdivani <georgemdivani.gm@gmail.com>
2018-03-07 14:56:43 +0400

    map sources added/styles pulled out of bundle.js

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import ExspensifyDashboardPage from '../components/ExspensifyDashboardPage';
-import AddExspensePage from '../components/AddExpensePage';
-import EditExspensePage from '../components/EditExspensePage';
+import ExpensifyDashboardPage from '../components/ExpensifyDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import Header from '../components/Header';
 import NotFoundPage from '../components/NotFoundPage';
