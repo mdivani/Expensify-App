58588cca1a15aec237954953e612e3db71676aa5
mdivani <georgemdivani.gm@gmail.com>
2018-03-16 14:30:04 +0400

    added expenses summary component and tests cases

--- src/components/ExpensifyDashboardPage.js
+++ src/components/ExpensifyDashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpensesList from './ExpensesList';
 import ExpenseListFilter from './ExpenseListFilter';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpensifyDashboardPage = () => (
     <div>
         <ExpenseListFilter />
         <ExpensesList />
+        <ExpensesSummary />
     </div>
 );
 
