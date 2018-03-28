eb8242bbe4b729aa67b825b39f8f40d978bb4e67
mdivani <georgemdivani.gm@gmail.com>
2018-03-28 17:36:23 +0400

    added private and public routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import { startLogout } from '../actions/auth';
+import { connect } from 'react-redux';
 
-const Header = () => (
+export const Header = ({startLogout}) => (
     <header>
         <h1>Expensify</h1>
-        <ul>
-            <li>
-                <NavLink to='/' activeClassName='is-active' exact={true}>Dashboard</NavLink>
-            </li>
-            <li>
-                <NavLink to='/create' activeClassName='is-active'>Create</NavLink>
-            </li>
-        </ul>
+        <NavLink to='/dashboard' activeClassName='is-active'>Dashboard</NavLink>
+        <NavLink to='/create' activeClassName='is-active'>Create</NavLink>
+        <button onClick={startLogout}>Logout</button>
     </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,6 +1,6 @@
 import expensesReducer from '../../reducers/expenses';
 import expenses from '../fixtures/expenses';
-import { stat } from 'fs';
+import { state } from 'fs';
 
 test('should set default state', () => {
     const state = expensesReducer(undefined, {type: '@@INIT'});
