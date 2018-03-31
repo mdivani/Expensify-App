34b8b2f5376912b789b54ae8f5f597d10ff8b273
mdivani <georgemdivani.gm@gmail.com>
2018-03-31 08:45:46 +0400

    login page styles added

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,12 @@ import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => {
     return (
-        <div>
-            <button onClick={startLogin}>Login</button>
+        <div className='box_layout'>
+            <div className='box_layout__box'>
+                <h1 className='box_layout__title'>Expensify</h1>
+                <p>time to take your expenses under control.</p>
+                <button className='button' onClick={startLogin}>Login with Google</button>
+            </div>
         </div>
     )
 }
